f36958612aee7ba098849605656c4c5e97a10563
Affan Haq <haqgamer66@gmail.com>
2020-11-11 13:55:58 -0500

    added login & register tests

---
+++ stubs/tests/Feature/Auth/LoginTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Tests\Feature\Auth;
+
+use App\Http\Livewire\Auth\Login;
+use App\Models\User;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Livewire\Livewire;
+use Tests\TestCase;
+
+class LoginTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function can_login_page_be_seen()
+    {
+        $this->get('/login')
+            ->assertStatus(200);
+    }
+
+    /** @test */
+    public function can_user_login()
+    {
+        $user = User::factory()->create();
+
+        Livewire::test(Login::class)
+            ->set('email', $user->email)
+            ->set('password', 'password')
+            ->call('login')
+            ->assertRedirect(RouteServiceProvider::HOME);
+
+        $this->assertAuthenticated();
+    }
+
+    /** @test */
+    public function can_user_not_login_with_wrong_password()
+    {
+        $user = User::factory()->create();
+
+        Livewire::test(Login::class)
+            ->set('email', $user->email)
+            ->set('password', 'bad-password')
+            ->call('login')
+            ->assertHasErrors('email');
+
+        $this->assertGuest();
+    }
+}
